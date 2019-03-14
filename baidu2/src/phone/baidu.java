a9f3041f08b53660b87592ce2ab7777686317011
daemon <2024272244@qq.com>
2019-03-14 09:28:24 +0800

    baidu

---
+++ baidu2/src/phone/baidu.java
@@ -0,0 +1,19 @@
+package phone;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class baidu {
+	@Test
+	public void mobile() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "./tools/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://baidu.com");
+		driver.findElement(By.cssSelector("#kw")).sendKeys("hello world");
+		driver.findElement(By.xpath("//*[@id=\"su\"]")).click();
+	}
+	
+}
